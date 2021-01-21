75e8fd9976ffd1ac02e0f10b0e84e0273dfb82f9
Duncan <duncanmarten@yahoo.com>
2021-01-21 11:51:28 -0600

    bugfixes#2

--- public/service-worker.js
+++ public/service-worker.js
@@ -49,79 +49,38 @@ self.addEventListener('activate', function(e) {
 });
 
 // Intercept fetch requests
-// self.addEventListener('fetch', function(e) {
-//     if (e.request.url.includes('/api/')) {
-//         e.respondWith(
-//             caches.open(DATA_CACHE_NAME)
-//                 .then(cache => {
-//                     return fetch(e.request)
-//                         .then (response => {
-//                             if (response.status === 200) {
-//                                 cache.put(e.request.url, response.clone());
-//                             }
-//                             return response;
-//                         })
-//                         .catch(err => {
-//                             console.log(err);
-//                             return cache.match(e.request);
-//                         })
-//                 })
-//                 .catch(err => console.log(err))
-//         );
-//         return;
-//     }
-
-//     e.respondWith(
-//         fetch(e.request).catch(function() {
-//             return caches.match(e.request)
-//                 .then(function(response) {
-//                     if(response) {
-//                         return response;
-//                     } else if (e.request.headers.get('accept').includes('text/html')) {
-//                         return caches.match('/');
-//                     }
-//                 });
-//         })
-//     );
-// });
-
-// Intercept fetch requests
-self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.includes('/api/')) {
-        evt.respondWith(
-          caches
-            .open(DATA_CACHE_NAME)
-            .then(cache => {
-              return fetch(evt.request)
-                .then(response => {
-                  // If the response was good, clone it and store it in the cache.
-                  if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                  }
-    
-                  return response;
+self.addEventListener('fetch', function(e) {
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(e.request)
+                        .then (response => {
+                            if (response.status === 200) {
+                                cache.put(e.request.url, response.clone());
+                            }
+                            return response;
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            return cache.match(e.request);
+                        })
                 })
-                .catch(err => {
-                  // Network request failed, try to get it from the cache.
-                  return cache.match(evt.request);
-                });
-            })
-            .catch(err => console.log(err))
+                .catch(err => console.log(err))
         );
-    
         return;
     }
 
-    evt.respondWith(
-        fetch(evt.request).catch(function() {
-          return caches.match(evt.request).then(function(response) {
-            if (response) {
-              return response;
-            } else if (evt.request.headers.get('accept').includes('text/html')) {
-              // return the cached home page for all requests for html pages
-              return caches.match('/');
-            }
-          });
+    e.respondWith(
+        fetch(e.request).catch(function() {
+            return caches.match(e.request)
+                .then(function(response) {
+                    if(response) {
+                        return response;
+                    } else if (e.request.headers.get('accept').includes('text/html')) {
+                        return caches.match('/');
+                    }
+                });
         })
     );
 });
